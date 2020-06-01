5c57052789ac4eb99893b5bf1a01b71baf1c0b83
Anton_U <utkin6567@gmail.com>
2020-06-01 23:12:49 +0300

    Lesson7

--- Task2.py
+++ Task2.py
@@ -1,26 +1,82 @@
-'''Реализовать класс Road (дорога), в котором определить атрибуты: length (длина),
-width (ширина). Значения данных атрибутов должны передаваться при создании экземпляра класса.
-Атрибуты сделать защищенными. Определить метод расчета массы асфальта,
-необходимого для покрытия всего дорожного полотна.
-Использовать формулу: длина*ширина*масса асфальта для покрытия
-одного кв метра дороги асфальтом, толщиной в 1 см*число см толщины полотна.
-Проверить работу метода.
-Например: 20м*5000м*25кг*5см = 12500 т
-'''
-class Road:
-    def __init__(self, _length, _width, weight, thickness):
-        self._length = _length
-        self._width = _width
-        self.weight = weight
-        self.thickness = thickness
+'''Реализовать проект расчета суммарного расхода ткани на производство одежды.
+Основная сущность (класс) этого проекта — одежда,
+которая может иметь определенное название. К типам одежды в этом проекте
+относятся пальто и костюм. У этих типов одежды существуют параметры:
+размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
+Для определения расхода ткани по каждому типу одежды использовать формулы:
+для пальто (V/6.5 + 0.5), для костюма (2*H + 0.3).
+Проверить работу этих методов на реальных данных.
+Реализовать общий подсчет расхода ткани.
+Проверить на практике полученные на этом уроке знания:
+реализовать абстрактные классы для основных классов проекта, проверить на практике
+работу декоратора @property.'''
+from abc import ABC, abstractmethod
 
-    def mass(self):
-        leng = self._length
-        wid = self._width
-        weig = self.weight
-        thic = self.thickness
-        total = leng * wid * weig * thic / 1000
-        return print(f'\n {leng}м*{wid}м*{weig}кг*{thic}см = ', int(total), 'т')
+class Clothes(ABC):
+    def __init__(self, value):
+        self.value = value
 
-my_road = Road(20, 5000, 25, 5)
-my_road.mass()
+    @abstractmethod
+    def my_method_1(self):
+        print('Тип одежды: ', end='')
+
+    @abstractmethod
+    def my_method_2(self):
+        print('Параметры одежды: ', end='')
+
+    @abstractmethod
+    def my_method_3(self):
+        print('Расход ткани: ', end='')
+
+class Coat(Clothes):
+    def my_method_1(self):
+        super().my_method_1()
+        print('Пальто')
+
+    def my_method_2(self):
+        super().my_method_2()
+        print('Размер')
+
+    def my_method_3(self):
+        super().my_method_3()
+        return float(self.value) / 6.5 + 0.5
+
+
+class Suit(Clothes):
+    def my_method_1(self):
+        super().my_method_1()
+        print('Костюм')
+
+    def my_method_2(self):
+        super().my_method_2()
+        print('Рост')
+
+    def my_method_3(self):
+        super().my_method_3()
+        return 2 * float(self.value) + 0.3
+
+
+class Total:
+    def __init__(self, size, height):
+        self.size = size
+        self.height = height
+
+    def sum(self):
+        return (self.size / 6.5 + 0.5) + (2 * self.height + 0.3)
+
+
+size_coat = 40
+size_suit = 46
+
+coat = Coat(size_coat)
+coat.my_method_1()
+coat.my_method_2()
+print('%.2f' % coat.my_method_3())
+
+suit = Suit(size_suit)
+suit.my_method_1()
+suit.my_method_2()
+print('%.2f' % suit.my_method_3())
+
+total = Total(size_coat, size_suit)
+print('\nОбщий расход ткани: %.2f' % total.sum())
